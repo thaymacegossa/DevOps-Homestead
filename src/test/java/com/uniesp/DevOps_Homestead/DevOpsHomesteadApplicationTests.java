0e8d5cfcb12c0a4a25553068ede67ee264872d04
Simon Lucena <thaymacegossa@gmail.com>
2026-05-25 14:41:52 -0300

    Update DevOpsHomesteadApplicationTests.java
    Correção de import

--- src/test/java/com/uniesp/DevOps_Homestead/DevOpsHomesteadApplicationTests.java
+++ src/test/java/com/uniesp/DevOps_Homestead/DevOpsHomesteadApplicationTests.java
@@ -5,8 +5,6 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.uniesp.DevOps_Homestead.DevOpsHomesteadApplication;
-
 class DevOpsHomesteadApplicationTests {
 
 	@Test
