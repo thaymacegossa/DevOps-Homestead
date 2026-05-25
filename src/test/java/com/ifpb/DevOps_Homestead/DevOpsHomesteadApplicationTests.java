f3696abbaeea1020eb6158be19de7ddb58f070ac
Deborah <deborahacvigliar@gmail.com>
2026-05-24 23:43:47 -0300

    Update DevOpsHomesteadApplicationTests.java

--- src/test/java/com/ifpb/DevOps_Homestead/DevOpsHomesteadApplicationTests.java
+++ src/test/java/com/ifpb/DevOps_Homestead/DevOpsHomesteadApplicationTests.java
@@ -1,13 +1,15 @@
 package com.ifpb.DevOps_Homestead;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@SpringBootTest
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 class DevOpsHomesteadApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void devePossuirAnotacaoSpringBootApplication() {
+		assertTrue(DevOpsHomesteadApplication.class.isAnnotationPresent(SpringBootApplication.class));
 	}
 
 }
